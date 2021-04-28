86d81ef4a68ac3eb91b50f6c65ff6900a6eea00a
snowfish <63844215+fish-snow@users.noreply.github.com>
2021-04-28 20:40:08 +0800

    Site updated: 2021-04-28 20:40:06

--- js/fold2.js
+++ js/fold2.js
@@ -1,19 +1,33 @@
-var card_category_list = document.getElementsByClassName("card-category-list child"); 
-var item = document.getElementsByClassName("card-category-list-item"); 
-function toggle(t) {     
-var display = t.parentElement.nextSibling.style.display;     
-    if (display == "none") {         
+var card_category_list = document.getElementsByClassName("card-category-list child"); // 获取元素集合
+var item = document.getElementsByClassName("card-category-list-item"); // 获取元素集合
+
+function menu_over(t) {     
+    var display = t.parentElement.nextSibling.style.display;     // 返回t的父元素节点名称，返回同胞
+    if(display == "none"){
         t.parentElement.nextSibling.style.display = "block";         
-        t.parentElement.nextSibling.style.height = "100%";         
-        t.className = t.className.replace("fa-chevron-up", "fa-chevron-down");     
-    } 
-        else {        
-             t.parentElement.nextSibling.style.display = "none";        
-              t.className = t.className.replace("fa-chevron-down", "fa-chevron-up");     
-            }
-        }  
-        for (var i = 0; i < card_category_list.length; i++) {     
-            card_category_list[i].style.display = "none";     
-            card_category_list[i].style.transition = "all 1s";     
-            card_category_list[i].previousSibling.innerHTML += '<i class="fa fa-chevron-up menus-expand  menus-closed" aria-hidden="true" style="margin-left:20px;" onclick="toggle(this)"></i>'; 
+        t.parentElement.nextSibling.style.height = "0%";         
+        t.className = t.className.replace("fa-chevron-right", "fa-chevron-down");   
+    }  
+}
+
+function menu_out(t){
+    var display = t.parentElement.nextSibling.style.display;
+    if(display != "none"){
+        t.parentElement.nextSibling.style.display = "none";
+        t.className = t.className = t.className.replace("fa-chevron-down", "fa-chevron-right");
     }
+}
+
+function delete_href(){
+    var card_category_list_link = document.getElementsByClassName("card-category-list-link"); // 返回带有超链接的集合
+    for(var i = 0; i < card_category_list_link.length; i++){
+        // alert(card_category_list_link[i])
+        card_category_list_link[i].removeAttribute("href");
+    }
+}
+// delete_href();
+for (var i = 0; i < card_category_list.length; i++) {   
+    card_category_list[i].style.display = "none";     
+    card_category_list[i].style.transition = "all 1s";     
+    card_category_list[i].previousSibling.innerHTML += '<i class="fa fa-chevron-right menus-expand  menus-close" aria-hidden="false" onmouseover="menu_over(this)" onmouseout="menu_out(this)" style="margin-left:15px;"></i>'; 
+}
